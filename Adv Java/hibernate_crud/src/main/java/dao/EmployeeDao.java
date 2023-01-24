c30075a0886fe5b59067798fe8ffbf53345d7398
dac <dac@dypm.org>
2023-01-24 18:50:53 +0530

    hibernate code

---
+++ Adv Java/hibernate_crud/src/main/java/dao/EmployeeDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+import java.util.List;
+
+import pojos.Department;
+import pojos.Employee;
+
+public interface EmployeeDao {
+
+	// add a method to save emp details
+	String addEmpDetails(Employee newEmp);
+
+	// add a method to list emps from a specific dept n earning sal > specific sal
+	List<Employee> getEmpsByDeptAndSalary(Department dept, double minSal);
+
+	// list of emp with specific details
+	List<Employee> getEmpWithSpecificDetails();
+}
---
+++ Adv Java/hibernate_crud/src/main/java/tester/Test.java
@@ -0,0 +1,32 @@
+package tester;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
+import dao.EmployeeDaoImpl;
+import pojos.Department;
+import pojos.Employee;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		EmployeeDaoImpl newEmp = new EmployeeDaoImpl();
+
+		// 1 add emp
+//		System.out.println("Enter emp:firstName, lastName, dept, salary, dob, isPermanent");
+//		Employee emp = new Employee(in.next(), in.next(), Department.valueOf(in.next()), in.nextDouble(),
+//				LocalDate.parse(in.next()), in.nextBoolean());
+//		System.out.println(newEmp.addEmpDetails(emp));
+
+		// 2 list of emp by dept and salary
+//		System.out.println("Enter dept and sal");
+//		List<Employee> list = newEmp.getEmpsByDeptAndSalary(Department.valueOf(in.next()), in.nextDouble());
+//		list.forEach(System.out::println);
+
+		// 3 list with specific details
+		List<Employee> list = newEmp.getEmpWithSpecificDetails();
+		list.forEach(System.out::println);
+	}
+}
